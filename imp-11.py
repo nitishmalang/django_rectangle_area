317e49d6837ff1e959f52606493fd79b7769a195
Nitish Malang <71919457+nitishmalang@users.noreply.github.com>
2023-09-24 13:17:03 +0530

    Update imp-11.py

--- imp-11.py
+++ imp-11.py
@@ -1,9 +1,4 @@
-# dingo : a python library for metabolic networks sampling and analysis
-# dingo is part of GeomScale project
-
-# Copyright (c) 2021 Apostolos Chalkis
-
-# Licensed under GNU LGPL.3, see LICENCE file
+e
 
 import sys
 import numpy as np
