f2d4ecd89fda33765b9479fa2eae4a681fe9bdbb
Nitish Malang <71919457+nitishmalang@users.noreply.github.com>
2023-10-01 00:39:20 +0530

    Update implementation-2.py

--- implementation-2.py
+++ implementation-2.py
@@ -22,7 +22,7 @@ def fast_fba(lb, ub, S, c):
     Keyword arguments:
     lb -- lower bounds for the fluxes, i.e., a n-dimensional vector
     ub -- upper bounds for the fluxes, i.e., a n-dimensional vector
-    S -- the mxn stoichiometric matrix, s.t. Sv = 0
+    S -= 0
     c -- the linear objective function, i.e., a n-dimensional vector
     """
 
