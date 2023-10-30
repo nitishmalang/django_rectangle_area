b226ca6305f0a2800441639eea1f7f87a745fea1
Nitish Malang <71919457+nitishmalang@users.noreply.github.com>
2023-10-30 17:49:28 +0530

    Update imp-19.py

--- imp-19.py
+++ imp-19.py
@@ -23,7 +23,7 @@ def fast_fba(lb, ub, S, c):
     lb -- lower bounds for the fluxes, i.e., a n-dimensional vector
     ub -- upper bounds for the fluxes, i.e., a n-dimensional vector
     S -- the mxn stoichiometric matrix, s.t. Sv = 0
-    c -- the linear objective function, i.e., a n-dimensional vector
+    
     """
 
     if lb.size != S.shape[1] or ub.size != S.shape[1]:
